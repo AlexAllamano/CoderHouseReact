a04ae8b28d03b6d2a66d56f7da6cbacfcc1038aa
Alex Allamano <alexisallamano@hotmail.com>
2022-11-24 10:57:17 -0300

    Commit inicial

---
+++ src/components/button/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './styles.css'
+
+const Button = () => {
+   return(
+    <button className="button-primary">Dame Cliccccc</button>
+   )    
+}
+
+export default Button;
